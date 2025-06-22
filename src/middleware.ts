114867814267f500503072574e7319b38a75593a
재욱 <haensol976520@gmail.com>
2025-06-22 09:12:29 +0900

    feat: add page

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,6 +24,7 @@ export function middleware(request: NextRequest) {
     pathname !== "/home" &&
     pathname !== "/apply" &&
     pathname !== "/slogan" &&
+    pathname !== "/booking" &&
     !accessToken &&
     !refreshToken
   ) {
---
+++ src/views/booking/ui/BookingPage/index.tsx
@@ -0,0 +1,9 @@
+const BookingPage = () => {
+  return (
+    <div>
+      <h1>BookingPage</h1>
+    </div>
+  );
+};
+
+export default BookingPage; 
