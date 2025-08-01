e02d96822170479b1d27b8642d99b99519b89267
재욱 <haensol976520@gmail.com>
2025-08-01 18:52:08 +0900

    fix: 2025 광탈페 예선 다시보기

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -39,7 +39,7 @@ export default function Header() {
   const links = [
     { section: "SloganSecondSection", label: "2025 광탈페 슬로건" },
     // { section: "section3", label: "FaQ" },
-    { section: "PreliminaryFourthSection", label: "예선" },
+    { section: "PreliminaryFourthSection", label: "2025 광탈페 예선 다시보기" },
     { section: "ReservationFifthSection", label: "예매" },
     { section: "FinalsSixthSection", label: "본선" },
   ];
