4d2895dbafad19de8656251bc67515c6bba5df98
재욱 <haensol976520@gmail.com>
2025-08-01 18:53:03 +0900

    feat: 텍스트 수정

--- src/shared/ui/Header/index.tsx
+++ src/shared/ui/Header/index.tsx
@@ -40,7 +40,7 @@ export default function Header() {
     { section: "SloganSecondSection", label: "2025 광탈페 슬로건" },
     // { section: "section3", label: "FaQ" },
     { section: "PreliminaryFourthSection", label: "2025 광탈페 예선 다시보기" },
-    { section: "ReservationFifthSection", label: "예매" },
+    { section: "ReservationFifthSection", label: "본선 좌석예매" },
     { section: "FinalsSixthSection", label: "본선" },
   ];
 
