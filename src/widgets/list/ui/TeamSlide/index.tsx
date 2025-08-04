e46a8a2870380499151be9fb6b954efb4f56392f
bae080311 <s24024@gsm.hs.kr>
2025-08-04 15:18:31 +0900

    feat: team slide

---
+++ src/widgets/list/ui/TeamSlide/index.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/shared/ui";
+import { handleStatus } from "@/views/list/lib/handleStatus";
+
+interface TeamSlideProps {
+  setItem: (v: number) => void;
+  teamName: string;
+  voteCount: number;
+  status: "PENDING" | "ONGOING" | "FINISHED";
+}
+
+export default function TeamSlide({ teamName, status, voteCount }: TeamSlideProps) {
+  return (
+    <div className="w-full flex flex-col gap-[40px] h-full py-4">
+      <div className="flex gap-28 items-center">
+        <span className="text-gray-500 text-body2r">진행팀</span>
+        <h3 className="text-body1b">{teamName}</h3>
+      </div>
+      <div className="flex items-center gap-[32px]">
+        <span className="text-gray-500 text-body2r">투표상태</span>
+        <Button className="w-[80px]" varient="outline">
+          {handleStatus(status, "vote").label}
+        </Button>
+      </div>
+      <div className="flex items-center gap-[37px]">
+        <span className="text-gray-500 text-body2r">현재 투표 수</span>
+        <h3 className="text-body1b text-main-600">{voteCount}</h3>
+      </div>
+    </div>
+  );
+}
