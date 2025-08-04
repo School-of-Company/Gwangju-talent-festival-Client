0132b246e334da383d0665ceced84587058742c9
bae080311 <s24024@gsm.hs.kr>
2025-08-04 15:20:23 +0900

    feat: list page

---
+++ src/views/list/ui/ListView/index.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import MiniHeader from "@/shared/ui/MiniHeader";
+import { useGetTeams } from "../../model/useGetTeams";
+import { useGetVotes } from "../../model/useGetVotes";
+import { useState } from "react";
+import { Button } from "@/shared/ui";
+import { closeVote } from "@/entities/list/api/closeVote";
+import { openVote } from "@/entities/list/api/openVote";
+import { TeamCard, VoteCard, Wrapper } from "@/entities/list/ui";
+import { toast } from "sonner";
+import TeamSlide from "@/widgets/list/ui/TeamSlide";
+import { startCount } from "@/entities/list/api/startCount";
+
+export default function ListView() {
+  const [item, setItem] = useState(0);
+  const { data: votes, isError: votesIsError, error: votesError } = useGetVotes();
+  const { data: teams, isError: teamsIsError, error: teamsError } = useGetTeams();
+
+  if (votesIsError) toast.error(votesError.message ?? "투표를 불러오는데 실패했습니다");
+  if (teamsIsError) toast.error(teamsError.message ?? "팀을 불러오는데 실패했습니다");
+  const currentId = votes && votes[item].vote_id.toString();
+  return (
+    <div className="px-80 w-full">
+      <MiniHeader label="관리자" />
+      <div className="flex flex-col gap-[48px]">
+        <div className="flex gap-[44px]">
+          <Wrapper label="현재 상태">
+            <TeamSlide
+              voteCount={votes?.[item].voteCount ?? 0}
+              setItem={setItem}
+              status={votes?.[item].status ?? "PENDING"}
+              teamName={votes?.[item].teamName ?? "팀"}
+            />
+          </Wrapper>
+          <div className="w-[40%]">
+            <Wrapper label="투표 제어">
+              <Button onClick={() => openVote(currentId!)}>투표시작</Button>
+              <Button varient="secondary" onClick={() => startCount(currentId!)}>
+                집계 시작
+              </Button>
+              <Button varient="third" onClick={() => closeVote(currentId!)}>
+                투표 완료
+              </Button>
+            </Wrapper>
+          </div>
+        </div>
+        <Wrapper label="팀">
+          <div className="flex gap-28 flex-wrap">
+            {teams?.map(v => {
+              return (
+                <TeamCard
+                  status={v.status}
+                  teamId={v.team_id}
+                  teamName={v.teamName}
+                  key={v.team_id}
+                />
+              );
+            })}
+          </div>
+        </Wrapper>
+        <Wrapper label="전체 투표 결과">
+          <div className="flex gap-28 flex-wrap">
+            {votes?.map(v => {
+              return (
+                <VoteCard
+                  voteCount={v.voteCount}
+                  teamName={v.teamName}
+                  voteId={v.vote_id}
+                  key={v.team_id}
+                  status={v.status}
+                />
+              );
+            })}
+          </div>
+        </Wrapper>
+      </div>
+    </div>
+  );
+}
