d906ea94c18cf564a1517f807af8c2b23444ae46
bae080311 <s24024@gsm.hs.kr>
2025-08-04 15:19:59 +0900

    fix: switch structure

--- src/views/list/lib/handleStatus.ts
+++ src/views/list/lib/handleStatus.ts
@@ -2,12 +2,24 @@ export const handleStatus = (
   status: "PENDING" | "ONGOING" | "FINISHED",
   type: "vote" | "performance",
 ) => {
-  switch (status) {
-    case "FINISHED":
-      return { label: "공연 완료", color: "#17C200" };
-    case "ONGOING":
-      return { label: type === "vote" ? "투표 중" : "공연 중", color: "#FE2F33" };
-    case "PENDING":
-      return { label: "대기 중", color: "#FEBD2F" };
+  switch (type) {
+    case "vote":
+      switch (status) {
+        case "FINISHED":
+          return { label: "투표 완료", color: "text-gray-800" };
+        case "ONGOING":
+          return { label: "투표 중", color: "text-main-600" };
+        case "PENDING":
+          return { label: "대기 중", color: "text-gray-400" };
+      }
+    case "performance":
+      switch (status) {
+        case "FINISHED":
+          return { label: "공연 완료", color: "text-#17C200", bg: "bg-#17C200" };
+        case "ONGOING":
+          return { label: "공연 중", color: "text-#FE2F33", bg: "bg-#FE2F33" };
+        case "PENDING":
+          return { label: "대기 중", color: "text-#FEBD2F", bg: "bg-#FEBD2F" };
+      }
   }
 };
