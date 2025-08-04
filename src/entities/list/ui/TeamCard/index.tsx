36abb0e402d55fad423906ffe406d16292a92b5e
bae080311 <s24024@gsm.hs.kr>
2025-08-04 15:20:09 +0900

    feat: team card

---
+++ src/entities/list/ui/TeamCard/index.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/shared/utils/cn";
+import { handleStatus } from "@/views/list/lib/handleStatus";
+
+interface TeamCardProps {
+  teamName: string;
+  teamId: number;
+  status: "PENDING" | "ONGOING" | "FINISHED";
+}
+
+export default function TeamCard({ status, teamName }: TeamCardProps) {
+  const handledStatus = handleStatus(status, "performance");
+  return (
+    <article
+      className={cn(
+        "px-24 max-w-[199px] w-full py-20 flex items-center rounded-lg flex-col gap-16",
+        status === "ONGOING" ? "bg-[rgba(254,47,51,0.10)]" : "bg-gray-50",
+        status === "ONGOING" && "border border-solid border-[#FE2F33]",
+      )}
+    >
+      <div className="flex gap-8">
+        <div className={cn("size-10 rounded-full", handledStatus.bg)} />
+        <p className={cn("text-caption2r", handledStatus.color)}>{handledStatus.label}</p>
+      </div>
+      <h3 className="text-body2b">{teamName}</h3>
+    </article>
+  );
+}
