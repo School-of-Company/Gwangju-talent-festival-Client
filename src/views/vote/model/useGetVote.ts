6bcfdda42a0ee020ee92425fbcaafa2a3d39ee82
bae080311 <s24024@gsm.hs.kr>
2025-08-05 23:58:29 +0900

    feat: return type

--- src/views/vote/model/useGetVote.ts
+++ src/views/vote/model/useGetVote.ts
@@ -1,8 +1,21 @@
 import { useQuery } from "@tanstack/react-query";
 import { getVote } from "../api/getVote";
 
+interface Response {
+  team_id: number;
+  teamName: string;
+  prev_team: {
+    team_id: number;
+    teamName: string;
+  };
+  next_team: {
+    team_id: number;
+    teamName: string;
+  };
+}
+
 export const useGetVote = () => {
-  return useQuery({
+  return useQuery<Response>({
     queryKey: ["vote"],
     queryFn: getVote,
     refetchInterval: 5000,
