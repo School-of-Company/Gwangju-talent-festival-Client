7dbc5ab17e8a3190521292da3508e78442437cd8
bae080311 <s24024@gsm.hs.kr>
2025-08-09 16:50:06 +0900

    feat: evaluation page

---
+++ src/views/evaluation/ui/EvaluationView/index.tsx
@@ -0,0 +1,19 @@
+import EvaluationCard from "@/entities/evaluation/ui/EvaluationCard";
+import Standard from "@/entities/evaluation/ui/Standard";
+import { Button } from "@/shared/ui";
+
+const Mock = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default function EvaluationView() {
+  return (
+    <div className="flex items-center flex-col">
+      <Standard />
+      <div className="px-24 flex flex-col gap-24 w-full">
+        {Mock.map(v => {
+          return <EvaluationCard key={v} />;
+        })}
+      </div>
+      <Button className="w-[420px] mt-[60px] mb-[70px]">확인</Button>
+    </div>
+  );
+}
