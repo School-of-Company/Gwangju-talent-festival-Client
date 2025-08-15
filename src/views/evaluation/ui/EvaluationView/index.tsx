7a4920e72b155f4eb251f6ae7eba3fab652b4f84
bae080311 <s24024@gsm.hs.kr>
2025-08-16 02:00:57 +0900

    fix: use data

--- src/views/evaluation/ui/EvaluationView/index.tsx
+++ src/views/evaluation/ui/EvaluationView/index.tsx
@@ -1,17 +1,29 @@
 import EvaluationCard from "@/entities/evaluation/ui/EvaluationCard";
 import Standard from "@/entities/evaluation/ui/Standard";
 import { Button } from "@/shared/ui";
-
-const Mock = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+import { useGetEvaluations } from "../../model/useGetEvaluation";
 
 export default function EvaluationView() {
+  const { data } = useGetEvaluations();
   return (
     <div className="flex items-center flex-col">
       <Standard />
       <div className="justify-center flex flex-col gap-24 w-full">
-        {Mock.map(v => {
-          return <EvaluationCard order={v} key={v} />;
-        })}
+        {data &&
+          data.map(v => {
+            return (
+              <EvaluationCard
+                completion_expression={v.completion_expression}
+                creativity_composition={v.creativity_composition}
+                judge_id={v.judge_id}
+                total_score={v.total_score}
+                stage_manner_performance={v.stage_manner_performance}
+                team_id={v.team_id}
+                team_name={v.team_name}
+                key={v.judge_id}
+              />
+            );
+          })}
       </div>
       <Button className="w-[420px] mt-[60px] mb-[70px]">확인</Button>
     </div>
