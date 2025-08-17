2c1119e407de19f1755761652e836ce97860657f
bae080311 <s24024@gsm.hs.kr>
2025-08-17 15:58:33 +0900

    fix: give value

--- src/entities/evaluation/api/saveScore.ts
+++ src/entities/evaluation/api/saveScore.ts
@@ -1,8 +1,17 @@
 import instance from "@/shared/lib/axios";
 
-export const saveScore = async (team_id: number) => {
+export const saveScore = async (
+  team_id: number,
+  stage_manner_performance: number,
+  completion_expression: number,
+  creativity_composition: number,
+) => {
   try {
-    const res = await instance.post("/judge/" + team_id);
+    const res = await instance.post("/judge/" + team_id, {
+      stage_manner_performance,
+      completion_expression,
+      creativity_composition,
+    });
     return res;
   } catch (error) {
     throw error;
