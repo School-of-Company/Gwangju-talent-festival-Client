a09ef2cc36bc5c9d63f2d3463a85f6cd0ac5f21b
bae080311 <s24024@gsm.hs.kr>
2025-08-18 14:07:02 +0900

    fix: handle data

--- src/views/list/ui/ListView/index.tsx
+++ src/views/list/ui/ListView/index.tsx
@@ -2,7 +2,6 @@
 
 import MiniHeader from "@/shared/ui/MiniHeader";
 import { useGetTeams } from "../../model/useGetTeams";
-import { useGetVotes } from "../../model/useGetVotes";
 import { useState } from "react";
 import { Button } from "@/shared/ui";
 import { closeVote } from "@/entities/list/api/closeVote";
@@ -10,16 +9,13 @@ import { openVote } from "@/entities/list/api/openVote";
 import { TeamCard, Wrapper } from "@/entities/list/ui";
 import { toast } from "sonner";
 import TeamSlide from "@/widgets/list/ui/TeamSlide";
-import { startCount } from "@/entities/list/api/startCount";
 
 export default function ListView() {
   const [item, setItem] = useState(0);
-  const { data: votes, isError: votesIsError, error: votesError } = useGetVotes();
-  const { data: teams, isError: teamsIsError, error: teamsError } = useGetTeams();
+  const { data, isError, error } = useGetTeams();
 
-  if (votesIsError) toast.error(votesError.message ?? "투표를 불러오는데 실패했습니다");
-  if (teamsIsError) toast.error(teamsError.message ?? "팀을 불러오는데 실패했습니다");
-  const currentId = votes && votes[item].vote_id.toString();
+  if (isError) toast.error(error.message ?? "팀을 불러오는데 실패했습니다");
+  const currentId = String(data?.[item].team_id) ?? "0";
   return (
     <div className="px-80 w-full">
       <MiniHeader label="관리자" />
@@ -27,27 +23,24 @@ export default function ListView() {
         <div className="flex gap-[44px]">
           <Wrapper label="현재 상태">
             <TeamSlide
-              voteCount={votes?.[item].voteCount ?? 0}
+              voteCount={data?.[item].star ?? 0}
               setItem={setItem}
-              status={votes?.[item].status ?? "PENDING"}
-              teamName={votes?.[item].teamName ?? "팀"}
+              status={data?.[item].status ?? "PENDING"}
+              teamName={data?.[item].teamName ?? "팀"}
             />
           </Wrapper>
           <div className="w-[40%]">
             <Wrapper label="투표 제어">
-              <Button onClick={() => currentId && openVote(currentId)}>투표시작</Button>
-              <Button variant="secondary" onClick={() => currentId && startCount(currentId)}>
-                집계 시작
-              </Button>
+              <Button onClick={() => currentId && openVote(currentId)}>투표 시작</Button>
               <Button variant="third" onClick={() => currentId && closeVote(currentId)}>
-                투표 완료
+                투표 마감
               </Button>
             </Wrapper>
           </div>
         </div>
         <Wrapper label="팀">
           <div className="flex gap-28 flex-wrap">
-            {teams?.map(v => {
+            {data?.map(v => {
               return (
                 <TeamCard
                   status={v.status}
