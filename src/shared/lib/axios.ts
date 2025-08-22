c7f849898c894b91000db164498ccb14cb1bee85
bae080311 <s24024@gsm.hs.kr>
2025-08-22 23:54:40 +0900

    fix: instance

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,4 +1,5 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
+import { getTokenFromCookie } from "../utils/auth";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -11,8 +12,12 @@ if (typeof window !== "undefined") {
 
     config.withCredentials = !isAuthPage;
 
+    const accessToken = getTokenFromCookie("accessToken");
+    if (accessToken) {
+      config.headers = config.headers ?? {};
+      (config.headers as Record<string, string>).authorization = `Bearer ${accessToken}`;
+    }
+
     return config;
   });
 }
-
-export default instance;
