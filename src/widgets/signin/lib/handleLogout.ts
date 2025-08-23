d75b3af4f45d4576d8e08904a040032f830199cc
bae080311 <s24024@gsm.hs.kr>
2025-08-23 18:23:25 +0900

    feat: handle log out

---
+++ src/widgets/signin/lib/handleLogout.ts
@@ -0,0 +1,14 @@
+import { toast } from "sonner";
+import { logout } from "../api/logout";
+import { clearTokens } from "@/shared/utils/auth";
+
+export const handleLogout = async () => {
+  const res = await logout();
+  if (res.status === 200) {
+    toast.success("로그아웃 되었습니다");
+    clearTokens();
+    window.location.href = "/signin";
+  } else {
+    toast.error("로그아웃 실패하셨습니다");
+  }
+};
