45dfdb04587b628b15afb620a51f8b5ed58d096a
bae080311 <s24024@gsm.hs.kr>
2025-08-24 01:29:32 +0900

    fix: value

--- src/entities/list/consts/status.ts
+++ src/entities/list/consts/status.ts
@@ -1,7 +1,7 @@
 export const STATUS = {
-  PENDING: "PENDING",
-  ONGOING: "ONGOING",
-  FINISHED: "FINISHED",
+  PENDING: "대기 중",
+  ONGOING: "공연 중",
+  FINISHED: "공연 완료",
 } as const;
 
 export type StatusType = keyof typeof STATUS;
