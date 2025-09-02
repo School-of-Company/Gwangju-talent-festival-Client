913abd2f58f3196fc66facafb33c813d969c645c
재욱 <haensol976520@gmail.com>
2025-09-02 12:23:00 +0900

    fix: next.image src에 빈 문자열 안돼서 조건부 렌더링으로 변경

--- src/entities/home/ui/MemberCard/index.tsx
+++ src/entities/home/ui/MemberCard/index.tsx
@@ -17,13 +17,15 @@ export const MemberCard = ({ githubID }: MemberCardProps) => {
         "flex items-center w-[18.4375rem] h-[7rem] rounded-xl border border-[#B2B2B2] bg-white p-5 gap-1 space-x-4 flex-shrink-0 cursor-pointer",
       )}
     >
-      <Image
-        src={data?.avatar_url ?? ""}
-        width={72}
-        height={72}
-        alt={data?.name ?? ""}
-        className={cn("rounded-full")}
-      />
+      {data?.avatar_url ? (
+        <Image
+          src={data.avatar_url}
+          width={72}
+          height={72}
+          alt={data?.name ?? ""}
+          className={cn("rounded-full")}
+        />
+      ) : null}
       <div>
         <p className={cn("text-[1.5rem]/[2rem] font-medium")}>{data?.name ?? "조수민"}</p>
         <p className={cn("text-[1.375rem]/[1.75rem] font-normal")}>{data?.email}</p>
