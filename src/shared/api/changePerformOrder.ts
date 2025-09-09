a41e251792e42c30a0c408440b86b59cdbb55081
bae080311 <s24024@gsm.hs.kr>
2025-09-09 12:02:17 +0900

    remove: return

--- src/shared/api/changePerformOrder.ts
+++ src/shared/api/changePerformOrder.ts
@@ -8,7 +8,7 @@ export const changePerformOrder = () => {
   es.onmessage = event => {
     try {
       const payload = JSON.parse(event.data);
-      return payload;
+      return payload.team_id;
     } catch (error) {
       throw error;
     }
@@ -21,8 +21,5 @@ export const changePerformOrder = () => {
   es.onerror = e => {
     toast.error("현재 공연 중인 팀 조회 실패");
     console.log(e);
-  };
-  return () => {
-    es.close();
   };
 };
