c0c5a1a282c5dbb0a4f7640ccadc129d6d838062
happytaeyoon <s23021@gsm.hs.kr>
2025-09-15 22:08:34 +0900

    fix:mobile 반응형 수정

--- src/views/signup/ui/SignupPage/index.tsx
+++ src/views/signup/ui/SignupPage/index.tsx
@@ -6,13 +6,13 @@ import Link from "next/link";
 
 const SignupPage = () => {
   return (
-    <div className={cn("flex flex-col items-center mt-52 h-screen w-full px-12")}>
+    <div className={cn("flex flex-col justify-center items-center h-screen w-full px-12")}>
       <div className={cn("w-full max-w-md flex flex-col items-center")}>
-        <div className={cn("mb-20")}>
+        <div className={cn("mb-20 mobile:mb-0")}>
           <Logo color={colors.main[600]} width={200} height={200} />
         </div>
         <SignupFormContainer />
-        <p className="text-body2r mt-28 text-gray-500">
+        <p className="text-body2r mt-28 text-gray-500 mobile:text-caption1r mobile:mt-10 mobile:self-start">
           이미 회원가입을 하셨나요?{" "}
           <Link href="/signin" className="text-main-600 hover:underline">
             로그인 하러가기
