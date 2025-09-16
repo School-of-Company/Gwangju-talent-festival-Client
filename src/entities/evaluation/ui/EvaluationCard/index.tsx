4314b53845208a4fed0ad45c617b418bf4ac4e7a
bae080311 <s24024@gsm.hs.kr>
2025-09-16 12:23:27 +0900

    fix: design

--- src/entities/evaluation/ui/EvaluationCard/index.tsx
+++ src/entities/evaluation/ui/EvaluationCard/index.tsx
@@ -1,167 +1,50 @@
 "use client";
 
-import { CheckIcon } from "@/shared/asset/svg/CheckIcon";
-import { Button } from "@/shared/ui";
-import { useCallback, useEffect, useState } from "react";
-import CustomDropdown from "../Dropdown";
 import { Score } from "@/views/evaluation/model/score";
-import { saveScore } from "../../api/saveScore";
-import { toast } from "sonner";
-import { colors } from "@/shared/utils/color";
 import { cn } from "@/shared/utils/cn";
-
-type Label = "completion_expression" | "creativity_composition" | "stage_manner_performance";
-
-type ValueType = {
-  write: boolean;
-  max: number;
-  show: boolean;
-  label: Label;
-};
-
-const scores: ValueType[] = [
-  { write: false, max: 40, show: false, label: "completion_expression" },
-  { write: false, max: 30, show: false, label: "creativity_composition" },
-  { write: false, max: 30, show: false, label: "stage_manner_performance" },
-];
+import { Button } from "@/shared/ui";
+import DownArrow from "@/shared/asset/svg/DownArrow";
+import UpArrow from "@/shared/asset/svg/UpArrow";
 
 export default function EvaluationCard({
-  judge_id,
+  // judge_id,
   stage_manner_performance,
   completion_expression,
   creativity_composition,
   total_score,
   team_id,
   team_name,
-  is_judged,
+  // is_judged,
   is_performed,
 }: Score) {
-  const [values, setValues] = useState<ValueType[]>(scores);
-  const [scoreValues, setScoreValues] = useState<Record<Label, number>>({
-    completion_expression: 0,
-    creativity_composition: 0,
-    stage_manner_performance: 0,
-  });
-  const [variant, setVariant] = useState<"submitted" | "active" | "disabled">("active");
-
-  useEffect(() => {
-    setScoreValues({
-      completion_expression: Number(completion_expression) || 0,
-      creativity_composition: Number(creativity_composition) || 0,
-      stage_manner_performance: Number(stage_manner_performance) || 0,
-    });
-    if (is_judged) {
-      setVariant("submitted");
-      return;
-    } else if (!is_performed) {
-      setVariant("disabled");
-    }
-  }, [
-    judge_id,
-    stage_manner_performance,
-    completion_expression,
-    creativity_composition,
-    total_score,
-    team_id,
-    is_judged,
-    is_performed,
-  ]);
-
-  const handleSave = useCallback(async () => {
-    const { completion_expression, creativity_composition, stage_manner_performance } = scoreValues;
-    const res = await saveScore(
-      team_id,
-      Number(stage_manner_performance) || 0,
-      Number(completion_expression) || 0,
-      Number(creativity_composition) || 0,
-    );
-    if (res.status === 200) {
-      toast.success("심사 내용이 저장되었습니다");
-      setVariant("submitted");
-    } else {
-      toast.error("심사 내용 저장에 실패하였습니다");
-      setVariant("active");
-    }
-  }, [team_id, scoreValues]);
-
-  const changeActive = useCallback(() => {
-    setVariant("active");
-  }, []);
-
   return (
-    <ul
+    <div
       className={cn(
-        "w-full text-body3b flex py-14 border items-center rounded-md border-gray-100 border-solid justify-between px-24 pl-[80px]",
+        "text-body1b grid grid-cols-6 py-14 border text-gray-300 mx-24 rounded-md border-gray-100 border-solid items-center text-center",
         !is_performed && "bg-#E9E9E9",
       )}
     >
-      <li className="text-main-600">{team_id + ". " + team_name}</li>
-      {values.map((v, i) => {
-        return v.write ? (
-          <input
-            className="w-[46.53px]"
-            key={v.label}
-            max={v.max}
-            min={0}
-            type="number"
-            value={scoreValues[v.label]}
-            onChange={e => {
-              const val = e.target.value === "" ? 0 : Number(e.target.value);
-
-              if (!Number.isNaN(val) && val <= v.max && val >= 0) {
-                setScoreValues(prev => ({ ...prev, [v.label]: val }));
-              }
-            }}
-            onBlur={() => {
-              setValues(prev =>
-                prev.map((item, idx) =>
-                  idx === i
-                    ? {
-                        ...item,
-                        write: false,
-                        show: false,
-                      }
-                    : item,
-                ),
-              );
-            }}
-            autoFocus
-          />
-        ) : (
-          <CustomDropdown
-            key={v.label}
-            value={scoreValues[v.label]}
-            max={v.max}
-            isOpen={v.show}
-            onToggle={() => {
-              setValues(prev =>
-                prev.map((item, idx) => (idx === i ? { ...item, show: !item.show } : item)),
-              );
-            }}
-            onSelect={selectedValue => {
-              setScoreValues(prev => ({ ...prev, [v.label]: Number(selectedValue) }));
-              setValues(prev =>
-                prev.map((item, idx) =>
-                  idx === i ? { ...item, write: false, show: false } : item,
-                ),
-              );
-            }}
-            onDoubleClick={() => {
-              setValues(prev =>
-                prev.map((item, idx) => (idx === i ? { ...item, write: true, show: false } : item)),
-              );
-            }}
-          />
-        );
-      })}
-      <Button
-        onClick={variant === "submitted" ? changeActive : handleSave}
-        variant={variant === "submitted" ? "third" : "default"}
-        className={cn("py-12 px-16 gap-12 w-[126px] justify-center flex items-center")}
-      >
-        <CheckIcon color={variant === "submitted" ? colors.main[600] : "white"} />
-        {Object.values(scoreValues).reduce((sum, n) => sum + Number(n || 0), 0)}
-      </Button>
-    </ul>
+      <div className="text-main-500 text-center">{team_id + ". " + team_name}</div>
+      <div className="flex items-center justify-center gap-8">
+        <span className="ml-12">{completion_expression}</span>
+        <DownArrow height={36} width={37} />
+        <UpArrow />
+      </div>
+      <div className="flex items-center justify-center gap-8">
+        <span className="ml-12">{creativity_composition}</span>
+        <DownArrow height={36} width={37} />
+        <UpArrow />
+      </div>
+      <div className="flex items-center justify-center gap-8">
+        <span className="ml-12">{stage_manner_performance}</span>
+        <DownArrow height={36} width={37} />
+        <UpArrow />
+      </div>
+      <div>{total_score}</div>
+      <div className="w-full flex justify-center px-24 gap-24">
+        <Button className="text-body2b w-1/2">저장</Button>
+        <Button className="text-body2b w-1/2">수정</Button>
+      </div>
+    </div>
   );
 }
--- src/entities/evaluation/ui/Standard/index.tsx
+++ src/entities/evaluation/ui/Standard/index.tsx
@@ -1,33 +1,29 @@
 export default function Standard() {
   return (
-    <ul
-      className="bg-gray-50 px-[40px
-
-    ] w-full mt-24 mb-20 text-body2b items-center flex justify-around text-center py-24"
-    >
-      <li>
+    <div className="bg-gray-50 px-[40px] w-full mt-24 mb-20 text-body2b grid grid-cols-6 text-center py-24 items-center">
+      <div>
         연번
         <br />
         (심사번호)
-      </li>
-      <li>
+      </div>
+      <div>
         완성도 및 표현력
         <br /> (40)
-      </li>
-      <li>
+      </div>
+      <div>
         창의력과 구성
         <br />
         (30)
-      </li>
-      <li>
+      </div>
+      <div>
         무대매너 및 퍼포먼스 등<br />
         (30)
-      </li>
-      <li>
+      </div>
+      <div>
         계<br />
         (100)
-      </li>
-      <li>저장 및 수정 버튼</li>
-    </ul>
+      </div>
+      <div>저장 및 수정 버튼</div>
+    </div>
   );
 }
