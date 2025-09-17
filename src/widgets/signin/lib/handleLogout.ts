92efa4e704efe2cc209db27615a7154b9d3b2fde
재욱 <haensol976520@gmail.com>
2025-09-17 18:06:40 +0900

    fix: 로그아웃 API 안쓰게 변경

--- src/widgets/signin/lib/handleLogout.ts
+++ src/widgets/signin/lib/handleLogout.ts
@@ -1,18 +1,15 @@
 import { toast } from "sonner";
-import { logout } from "../api/logout";
 import { clearTokens } from "@/shared/utils/auth";
 
 export const handleLogout = async () => {
   try {
-    await logout();
-    toast.success("로그아웃 되었습니다");
-  } catch (error) {
-    console.error(error);
-    toast.error("로그아웃 중 오류가 발생했습니다. 다시 로그인해주세요.");
-  } finally {
     clearTokens();
     if (typeof window !== "undefined") {
       window.location.href = "/signin";
     }
+    toast.success("로그아웃 되었습니다");
+  } catch (error) {
+    console.error(error);
+    toast.error("로그아웃 중 오류가 발생했습니다. 다시 로그인해주세요.");
   }
 };
