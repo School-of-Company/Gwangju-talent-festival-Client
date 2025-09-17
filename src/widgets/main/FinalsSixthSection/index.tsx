bcf93d92019f0117272ddadb81459a6bc722e127
happytaeyoon <s23021@gsm.hs.kr>
2025-09-18 07:55:12 +0900

    fix:style 수정

--- src/widgets/main/FinalsSixthSection/index.tsx
+++ src/widgets/main/FinalsSixthSection/index.tsx
@@ -28,7 +28,7 @@ const FinalsSixthSection = () => {
             오시는 길
           </p>
           <div className="flex gap-20 mobile:flex-col ">
-            <div className="w-[40%] mobile:w-full">
+            <div className="w-[40%] mobile:w-full pb-16 flex flex-col justify-between">
               <p className={cn("text-body2r text-gray-500 mobile:text-caption2r mobile:py-8")}>
                 ∙ 일시: 2025.9.27.(토) <br />∙ 장소: 조선대학교 해오름관
               </p>
