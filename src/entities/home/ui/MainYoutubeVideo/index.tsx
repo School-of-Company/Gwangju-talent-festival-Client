f24d096caf54e1b742a8200f9c269899b2b7e450
koreahghg <koreahghg@gmail.com>
2026-03-27 14:39:03 +0900

    refactor: MainYoutubeVideo iframe 공통 속성 iframeProps로 추출

--- src/entities/home/ui/MainYoutubeVideo/index.tsx
+++ src/entities/home/ui/MainYoutubeVideo/index.tsx
@@ -3,23 +3,24 @@ export default function MainYoutubeVideo() {
   const youtubeId = "8iM_JB8u-Vo";
   const src = `https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&controls=0&playsinline=1&loop=1&playlist=${youtubeId}&modestbranding=1&rel=0`;
 
+  const iframeProps = {
+    src,
+    title: "Intro video",
+    allow: "autoplay; encrypted-media; picture-in-picture",
+    referrerPolicy: "strict-origin-when-cross-origin" as const,
+  };
+
   return (
     <>
       <iframe
         className="max-[1199px]:hidden absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[177.78vh] h-[100vh] min-w-full min-h-full pointer-events-none"
-        src={src}
-        title="Intro video"
-        allow="autoplay; encrypted-media; picture-in-picture"
-        referrerPolicy="strict-origin-when-cross-origin"
+        {...iframeProps}
       />
 
       <div className="min-[1200px]:hidden relative w-full aspect-video">
         <iframe
           className="absolute inset-0 w-full h-full pointer-events-none"
-          src={src}
-          title="Intro video"
-          allow="autoplay; encrypted-media; picture-in-picture"
-          referrerPolicy="strict-origin-when-cross-origin"
+          {...iframeProps}
         />
       </div>
 
