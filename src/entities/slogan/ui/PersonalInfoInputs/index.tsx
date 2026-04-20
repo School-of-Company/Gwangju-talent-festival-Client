00fcf84419e660a7505e811105c998cfd544896b
g-hyxn <s25024@gsm.hs.kr>
2026-04-20 22:55:16 +0900

    feat: PersonalInfoInputs에 isOutOfSchool prop 추가 및 조건부 렌더링 구현

--- src/entities/slogan/ui/PersonalInfoInputs/index.tsx
+++ src/entities/slogan/ui/PersonalInfoInputs/index.tsx
@@ -12,9 +12,10 @@ type PersonalInfoInputsProps = {
   onFieldChange: (
     field: keyof SloganFormValues,
   ) => (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isOutOfSchool: boolean;
 };
 
-const PersonalInfoInputs = ({ formValues, onFieldChange }: PersonalInfoInputsProps) => {
+const PersonalInfoInputs = ({ formValues, onFieldChange, isOutOfSchool }: PersonalInfoInputsProps) => {
   return (
     <>
       <Input
@@ -25,30 +26,32 @@ const PersonalInfoInputs = ({ formValues, onFieldChange }: PersonalInfoInputsPro
         label="이름"
         placeholder="이름을 입력해주세요"
       />
-      <div className="flex gap-24">
-        <Input
-          name="grade"
-          type="number"
-          value={formValues.grade}
-          onChange={onFieldChange("grade")}
-          label="학년"
-          placeholder="학년을 입력해주세요"
-        />
-        <Input
-          name="class"
-          type="number"
-          value={formValues.classroom}
-          onChange={onFieldChange("classroom")}
-          label="반"
-          placeholder="반을 입력해주세요"
-        />
-      </div>
+      {!isOutOfSchool && (
+        <div className="flex gap-24">
+          <Input
+            name="grade"
+            type="number"
+            value={formValues.grade}
+            onChange={onFieldChange("grade")}
+            label="학년"
+            placeholder="학년을 입력해주세요"
+          />
+          <Input
+            name="class"
+            type="number"
+            value={formValues.classroom}
+            onChange={onFieldChange("classroom")}
+            label="반"
+            placeholder="반을 입력해주세요"
+          />
+        </div>
+      )}
       <Input
         type="number"
         name="phone"
         value={formValues.phone_number}
         onChange={onFieldChange("phone_number")}
-        label="전화번호"
+        label="전화번호(휴대폰번호)"
         placeholder="전화번호를 입력해주세요"
       />
     </>
