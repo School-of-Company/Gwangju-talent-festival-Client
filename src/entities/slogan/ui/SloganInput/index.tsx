6645566436c4ced754575d744f4082bae8ce2a39
g-hyxn <s25024@gsm.hs.kr>
2026-04-20 17:12:33 +0900

    refactor: SloganInput 컴포넌트 구조 변경 및 Inform SVG 추가

--- src/entities/slogan/ui/SloganInput/index.tsx
+++ src/entities/slogan/ui/SloganInput/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Input } from "@/shared/ui";
-import CountLength from "../CountLength";
+import Inform from "@/shared/asset/svg/Inform";
 
 type SloganInputProps = {
   value: string;
@@ -9,7 +9,7 @@ type SloganInputProps = {
 
 const SloganInput = ({ value, onChange }: SloganInputProps) => {
   return (
-    <CountLength length={value.length}>
+    <div className="flex flex-col gap-4">
       <Input
         type="text"
         value={value}
@@ -19,7 +19,14 @@ const SloganInput = ({ value, onChange }: SloganInputProps) => {
         label="슬로건 입력"
         placeholder="슬로건을 입력해주세요"
       />
-    </CountLength>
+      <div className="flex items-center justify-between">
+        <p className="flex items-center gap-8 text-body3r text-gray-500">
+          <Inform width={20} height={20} color="#a7a7a7" />본 결과물은 표절을 금지하며, 반드시 순수
+          창작물로 작성해야 합니다.
+        </p>
+        <span className="text-body3r text-gray-400">{value.length}/100</span>
+      </div>
+    </div>
   );
 };
 
