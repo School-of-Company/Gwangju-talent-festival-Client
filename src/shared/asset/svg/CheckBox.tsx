0191760ce4dc2fdc9e15091900a9564b60ec67c1
g-hyxn <s25024@gsm.hs.kr>
2026-04-21 08:34:50 +0900

    fix: CheckBox 미선택 시 체크 아이콘 fill none으로 수정

--- src/shared/asset/svg/CheckBox.tsx
+++ src/shared/asset/svg/CheckBox.tsx
@@ -20,7 +20,7 @@ export default function CheckBox({ checked, width = 20, height = 20 }: CheckBoxP
         fillRule="evenodd"
         clipRule="evenodd"
         d="M8.33842 11.7277L14.5146 5.45508L15.4545 6.40966L8.33842 13.6369L4.54541 9.78463L5.48531 8.83005L8.33842 11.7277Z"
-        fill={checked ? "white" : "#FF9644"}
+        fill={checked ? "white" : "none"}
       />
     </svg>
   );
