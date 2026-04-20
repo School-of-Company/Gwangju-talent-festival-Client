4b3b6bf4b7b2c4e91c3bd18265dd211b8493988c
g-hyxn <s25024@gsm.hs.kr>
2026-04-20 20:48:28 +0900

    feat: postSlogan 함수에 isOutOfSchool 매개변수 추가 및 데이터 처리 로직 수정

--- src/entities/slogan/api/postSlogan.ts
+++ src/entities/slogan/api/postSlogan.ts
@@ -1,12 +1,17 @@
 import instance from "@/shared/lib/axios";
 import { SloganFormValues } from "../model/schema";
 
-export const postSlogan = async (data: SloganFormValues) => {
-  const { phone_number, classroom, grade, ...rest } = data;
+export const postSlogan = async (data: SloganFormValues, isOutOfSchool: boolean) => {
+  const { phone_number, classroom, grade, birthday, school, ...rest } = data;
+
+  const birthDate = birthday ? birthday.replace(/\s*\/\s*/g, "-") : undefined;
+
   return await instance.post("/slogan", {
     ...rest,
     phoneNumber: phone_number,
-    grade: Number(grade),
-    classNum: Number(classroom),
+    schoolStatus: isOutOfSchool ? "OUT_OF_SCHOOL" : "ENROLLED",
+    ...(isOutOfSchool
+      ? { birthDate }
+      : { school, grade: Number(grade), classNum: Number(classroom) }),
   });
 };
