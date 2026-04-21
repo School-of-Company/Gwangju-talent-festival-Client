0f8542fccd4beb2b04b9e28d9358891d44f6c8f3
moonkanghyeon <s25024@gsm.hs.kr>
2026-04-21 16:22:00 +0900

    fix: CheckBox 미체크 상태 fill 색상 수정

--- src/shared/asset/svg/CheckBox.tsx
+++ src/shared/asset/svg/CheckBox.tsx
@@ -6,7 +6,13 @@ type CheckBoxProps = {
 
 export default function CheckBox({ checked, width = 20, height = 20 }: CheckBoxProps) {
   return (
-    <svg width={width} height={height} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
       <rect
         x="0.5"
         y="0.5"
@@ -20,7 +26,7 @@ export default function CheckBox({ checked, width = 20, height = 20 }: CheckBoxP
         fillRule="evenodd"
         clipRule="evenodd"
         d="M8.33842 11.7277L14.5146 5.45508L15.4545 6.40966L8.33842 13.6369L4.54541 9.78463L5.48531 8.83005L8.33842 11.7277Z"
-        fill={checked ? "white" : "none"}
+        fill={checked ? "white" : "#FF9644"}
       />
     </svg>
   );
