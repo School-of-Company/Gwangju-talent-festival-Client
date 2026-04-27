452cb6b7889835b9273ca97639291002a6927357
koreahghg <koreahghg@gmail.com>
2026-04-27 23:46:53 +0900

    fix: MainYoutubeVideo YouTube UI 오버플로우 클리핑 및 그라디언트 처리

--- src/entities/home/ui/MainYoutubeVideo/index.tsx
+++ src/entities/home/ui/MainYoutubeVideo/index.tsx
@@ -2,41 +2,30 @@ import { cn } from "@/shared/utils/cn";
 
 export default function MainYoutubeVideo() {
   const youtubeId = "8iM_JB8u-Vo";
-  const src = `https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&controls=0&playsinline=1&loop=1&playlist=${youtubeId}&modestbranding=1&rel=0`;
-
-  const iframeProps = {
-    src,
-    title: "Intro video",
-    allow: "autoplay; encrypted-media; picture-in-picture",
-    referrerPolicy: "strict-origin-when-cross-origin" as const,
-  };
-
   return (
     <>
-      <iframe
-        className={cn(
-          "max-[1199px]:hidden",
-          "absolute",
-          "top-1/2",
-          "left-1/2",
-          "-translate-x-1/2",
-          "-translate-y-1/2",
-          "w-[177.78vh]",
-          "h-[100vh]",
-          "min-w-full",
-          "min-h-full",
-          "pointer-events-none",
-        )}
-        {...iframeProps}
-      />
-
-      <div className={cn("min-[1200px]:hidden", "relative", "w-full", "aspect-video")}>
+      <div className={cn("absolute", "inset-0", "overflow-hidden")}>
         <iframe
-          className={cn("absolute", "inset-0", "w-full", "h-full", "pointer-events-none")}
-          {...iframeProps}
+          className={cn(
+            "absolute",
+            "top-1/2",
+            "left-1/2",
+            "-translate-x-1/2",
+            "-translate-y-1/2",
+            "w-[177.78vh]",
+            "h-[56.25vw]",
+            "min-w-full",
+            "min-h-[110%]",
+            "pointer-events-none",
+          )}
+          src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&controls=0&playsinline=1&loop=1&playlist=${youtubeId}&modestbranding=1&rel=0&iv_load_policy=3&disablekb=1&fs=0&cc_load_policy=0`}
+          title="Intro video"
+          allow="autoplay; encrypted-media; picture-in-picture"
+          referrerPolicy="strict-origin-when-cross-origin"
         />
       </div>
 
+      <div className={cn("absolute", "inset-0", "pointer-events-none")} />
       <div
         className={cn(
           "absolute",
@@ -45,6 +34,15 @@ export default function MainYoutubeVideo() {
           "from-black",
           "via-black/30",
           "to-black/10",
+        )}
+      />
+      <div
+        className={cn(
+          "absolute",
+          "inset-0",
+          "bg-gradient-to-b",
+          "from-black/60",
+          "to-transparent",
         )}
       />
     </>
