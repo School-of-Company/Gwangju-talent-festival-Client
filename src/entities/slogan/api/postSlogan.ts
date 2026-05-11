5ac93501662fda342933e3f86891b6aa5e217cc5
koreahghg <koreahghg@gmail.com>
2026-05-11 23:55:56 +0900

    refactor: 슬로건 API 요청 로직 단순화

--- src/entities/slogan/api/postSlogan.ts
+++ src/entities/slogan/api/postSlogan.ts
@@ -4,12 +4,21 @@ import { SloganFormValues } from "../model/schema";
 export const postSlogan = async (data: SloganFormValues, isOutOfSchool: boolean) => {
   const { phone_number, classroom, grade, birthday, school, ...rest } = data;
 
+  if (isOutOfSchool) {
+    return await instance.post("/slogan", {
+      ...rest,
+      phoneNumber: phone_number,
+      schoolStatus: "OUT_OF_SCHOOL",
+      birthDate: birthday || undefined,
+    });
+  }
+
   return await instance.post("/slogan", {
     ...rest,
     phoneNumber: phone_number,
-    schoolStatus: isOutOfSchool ? "OUT_OF_SCHOOL" : "ENROLLED",
-    ...(isOutOfSchool
-      ? { birthDate: birthday || undefined }
-      : { school, grade: Number(grade), classNum: Number(classroom) }),
+    schoolStatus: "ENROLLED",
+    school,
+    grade: Number(grade),
+    classNum: Number(classroom),
   });
 };
