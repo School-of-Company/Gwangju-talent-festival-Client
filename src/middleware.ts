500d046594f502596a3b6ef0be6e8c84bea5fec4
g-hyxn <s25024@gsm.hs.kr>
2026-05-14 16:57:24 +0900

    feat: 미들웨어 삭제 되어있던 주석 부분 복구

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { publicPages, publicIn27 } from "@/shared/config/authConfig";
-import { festivalDate } from "@/shared/config/dateConfig";
+import { festivalDate, sloganStartDate, sloganEndDate } from "@/shared/config/dateConfig";
 
 export const config = {
   matcher: [
@@ -32,6 +32,11 @@ export function middleware(request: NextRequest) {
 
   const accessToken = request.cookies.get("accessToken")?.value;
   const refreshToken = request.cookies.get("refreshToken")?.value;
+
+  const now = new Date();
+  if (pathname === "/slogan" && (now < sloganStartDate || now > sloganEndDate)) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
 
   if (pathname === "/signin" && accessToken && refreshToken) {
     const nextParam = searchParams.get("next");
