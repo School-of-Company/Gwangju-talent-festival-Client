44cf7c9e4a2ec2f957a9acb5b5d4d9c6275e61f5
koreahghg <koreahghg@gmail.com>
2026-05-29 09:24:54 +0900

    fix: ApplyThirdSection 링크로 변경

--- src/shared/const/headerValues.ts
+++ src/shared/const/headerValues.ts
@@ -11,7 +11,7 @@ export const isHiddenPath = (pathname: string): boolean => {
 export const links = [
   { section: "SloganSecondSection", label: "2026 광탈페 슬로건" },
   // { section: "section3", label: "FaQ" },
-  { section: "PreliminaryFourthSection", label: "2025 광탈페 예선 다시보기" },
+  { section: "ApplyThirdSection", label: "참여 신청" },
   { section: "FinalsSixthSection", label: "2025 광탈페 본선 다시보기" },
   // { section: "ReservationFifthSection", label: "본선 수상팀 명단" },
 ];
