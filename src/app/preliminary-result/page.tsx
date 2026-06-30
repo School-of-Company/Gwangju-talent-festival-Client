fa95243e16651a990fcc75d199ce7f7573fad868
koreahghg <koreahghg@gmail.com>
2026-06-30 19:46:53 +0900

    feat: 메타데이터 제목 수정

--- src/app/preliminary-result/page.tsx
+++ src/app/preliminary-result/page.tsx
@@ -2,7 +2,7 @@ import PreliminaryResultView from "@/views/preliminary-result/ui/PreliminaryResu
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "예선 진출팀 발표 | 광주학생탈렌트페스티벌",
+  title: "광주학생탈렌트페스티벌",
   description: "2026 광탈페 光트로 예선 진출팀 24팀을 발표합니다.",
 };
 
